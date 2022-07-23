a90afc34a5afd3b035155d14e3ebc8b89a7c5eac
Jonas Wischeropp <jonas@wischeropp.de>
2022-07-23 19:06:30 +0200

    added sound to mornigstar ability

--- Assets/Features/HeroAbilities/MorningStarAbility/Scripts/MorningStarPhysicsController.cs
+++ Assets/Features/HeroAbilities/MorningStarAbility/Scripts/MorningStarPhysicsController.cs
@@ -13,44 +13,78 @@ public class MorningStarPhysicsController : MonoBehaviour
     [SerializeField] private float force;
     [SerializeField] private float groundingVelocity;
     [SerializeField] private float groundDrag;
+    
+    [Header("Audio")]
+    [SerializeField] private float soundCooldown;
+    [SerializeField] private AudioClip airSound;
+    [SerializeField] private AnimationCurve curve;
 
     private Rigidbody2D _rigidbody;
     private float _oldDrag;
+
+    private bool _dragged = false;
+
+    private AudioSource _audioSource;
+    private Coroutine _soundCoroutine;
     
     private void Awake()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
         _oldDrag = _rigidbody.drag;
 
-        ObservableDragTrigger observable = hitBox.TryGetComponent(typeof(ObservableDragTrigger), out var component) ?
-            (ObservableDragTrigger)component : hitBox.AddComponent<ObservableDragTrigger>();
-        observable.OnDragAsObservable().Subscribe(OnDrag).AddTo(this);
+        _audioSource = GetComponent<AudioSource>();
     }
 
     private void Start()
     {
+        ObservableDragTrigger observable = hitBox.TryGetComponent(typeof(ObservableDragTrigger), out var component) ?
+            (ObservableDragTrigger)component : hitBox.AddComponent<ObservableDragTrigger>();
+        observable.OnDragAsObservable().Subscribe(OnDrag).AddTo(this);
+        
         hitBox.GetComponent<PointerDownSensor>().SensorTriggered.Subscribe(_ =>
             {
-                enabled = false;
+                _dragged = false;
                 _rigidbody.drag = _oldDrag;
             })
             .AddTo(this);
-        hitBox.AddComponent<ObservablePointerUpTrigger>().OnPointerUpAsObservable().Subscribe(_ => enabled = true)
+        hitBox.AddComponent<ObservablePointerUpTrigger>().OnPointerUpAsObservable().Subscribe(_ => _dragged = true)
             .AddTo(this);
     }
 
     private void FixedUpdate()
     {
+        if (_dragged)
+            return;
+        
         if (_rigidbody.velocity.sqrMagnitude < groundingVelocity * groundingVelocity)
         {
             _rigidbody.drag = groundDrag;
-            // TODO play sound
-            Debug.Log("OnGround");
+            _dragged = true;
         }
     }
 
     private void OnDrag(PointerEventData data)
     {
         _rigidbody.AddForce((_rigidbody.position - (Vector2)stick.transform.position).normalized * force * Time.deltaTime);
+    }
+
+    private IEnumerator SoundPlayer()
+    {
+        while (true)
+        {
+            Debug.Log(_rigidbody.velocity.magnitude);
+            _audioSource.PlayOneShot(airSound, curve.Evaluate(_rigidbody.velocity.magnitude));
+            yield return new WaitForSeconds(soundCooldown);
+        }
+    }
+
+    private void OnEnable()
+    {
+        _soundCoroutine = StartCoroutine(SoundPlayer());
+    }
+
+    private void OnDisable()
+    {
+        StopCoroutine(_soundCoroutine);
     }
 }
